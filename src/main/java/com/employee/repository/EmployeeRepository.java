0a46c74e3013c900e8490a25da2090a226ecd479
PSP Tech Soluitons <psptechsolutions@gmail.com>
2024-02-08 14:43:11 +0530

    first commit

--- src/main/java/com/employee/repository/EmployeeRepository.java
+++ src/main/java/com/employee/repository/EmployeeRepository.java
@@ -1,13 +1,13 @@
 package com.employee.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.employee.model.Employee;
 
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+public interface EmployeeRepository extends MongoRepository<Employee, Long>{
 	
 	
 }
